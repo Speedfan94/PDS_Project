bc99282ecfc4b7af21cac29f8c79581c018feecf
Speedfan <43236732+Speedfan94@users.noreply.github.com>
2020-05-01 09:20:56 +0200

    chore/(nextike/io/output): update output function for new "Trips.csv"

--- nextbike/io/output.py
+++ nextbike/io/output.py
@@ -5,3 +5,9 @@ import pickle
 
 def save_model(model):
     pickle.dump(model, open(os.path.join(get_data_path(), "output/model.pkl"), 'wb'))
+
+
+def saveTrip(df):
+    # Save final df
+    print("Save csv")
+    df.to_csv("../nextbike/data/output/Trips.csv")
