95ae7960b0d488a83248fd243c3ca8cd26902054
Kevin W <kevin.blau2@gmail.com>
2020-05-05 12:00:53 +0200

    feat(nextbike\visualization\visualization.py): create skeleton for further excercises

---
+++ nextbike/visualization/visualization.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def visualize_plz(df):
+    # For the summer month (i.e., June, August, or September) with most trips, visualize the number
+    # of started trips per PLZ region (you’ll have to find geo data for that yourselves!) in a map.
+
+    print()
+
+
+def visualize_moment(df):
+    # For one moment in time, visualize the number of bikes at fixed stations meaningfully.
+
+    print()
+
+
+def visualize_heatmap(df):
+    # Create a heatmap based on an interesting aspect of the data, e.g., end locations of trips shortly
+    # before the start of a major public event.
+
+    print()
+
+
+def visualize_distribution(df):
+    # Visualize the distribution of trip lengths per month. Compare the distributions to normal
+    # distributions with mean and standard deviation as calculated before (1.d))
+
+    print()
+
+
+def visualize_more(df):
+    # These visualizations are the minimum requirement. Use more visualizations wherever it makes
+    # sense.
+
+    print()
