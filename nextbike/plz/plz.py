ab28f5ef01306f63d19e357421c6971c0d26ce00
Speedfan <43236732+Speedfan94@users.noreply.github.com>
2020-05-12 19:13:46 +0200

    feat/plz(nextbike/plz/plz.py)Adding plz to every row in the df

--- nextbike/plz/plz.py
+++ nextbike/plz/plz.py
@@ -3,10 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # import folium
 import seaborn as sns
-
+import shapely.geometry as shapely
 import json
 import vincenty
 from shapely.geometry import shape, Point
+
+plz_value = {}
 
 with open('../nextbike/data/input/postleitzahlen-nuremberg.geojson') as f:
     geo = json.load(f)
@@ -15,7 +17,9 @@ with open('../nextbike/data/input/postleitzahlen-nuremberg.geojson') as f:
 # Note reverse notation compared to before
 # searchpoint = Point(-0.109970, 51.529163)
 
-def plz(df):
+
+# Just for a single Point
+def plz1(df):
 
     searchpoint = Point(11.076750, 49.452030)
     # df_london_center["Longitude"], df_london_center["Latitude"])
@@ -27,12 +31,27 @@ def plz(df):
     return Point
 
 
-def plz2(df):
+def plz(df):
+
     for feature in geo['features']:
-        # x = stations_df_copy["Longitude"]
-        # y = stations_df_copy["Latitude"]
-        searchpoint = Point(df["Longitude_start"], df["Latitude_start"])
-        polygon = shape(feature['geometry'])
-        if polygon.contains(searchpoint):
-            df["plz"] = feature["properties"]["plz"]
+        if feature['geometry']['type'] == 'MultiPolygon':
+            plz_value[feature['properties']['plz']] = list(shapely.shape(feature['geometry']))
+        elif feature['geometry']['type'] == 'Polygon':
+            plz_value[feature['properties']['plz']] = shapely.shape(feature['geometry'])
+
+    df['plz_start'] = df.apply(lambda x: get_plz(x['Latitude_start'], x['Longitude_start']), axis=1)
     return df
+    # df_plz = df.groupby('plz_start', as_index=False)['Capacity'].count()
+    # df_boroughs.rename({'Capacity': 'Stations'}, axis=1, inplace=True)
+
+
+def get_plz(lat, lon):
+    p = shapely.Point(lon, lat)
+    for plz, shape in plz_value.items():
+        if type(shape) == list:
+            for poly in shape:
+                if poly.contains(p):
+                    return plz
+        elif shape.contains(p):
+            return plz
+
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='PDS_Project',
     version='0.0.1dev1',
     description="Semester Project - Programming Data Science",
-    author="Student",
+    author="team-we-hend-stroh-to-be-the-best-ulk-nudel",
     author_email="student@uni-koeln.de",
     packages=["nextbike"],
-    install_requires=['pandas', 'scikit-learn', 'click', 'vincenty', 'shapely'],
+    install_requires=['pandas', 'scikit-learn', 'click', 'vincenty', 'shapely', 'folium'],
     entry_points={
         'console_scripts': ['nextbike=nextbike.cli:main']
     }
