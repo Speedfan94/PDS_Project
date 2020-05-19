d29c3c107041c7a76a31aaefa24c3c955c98dc5f
Kevin W <kevin.blau2@gmail.com>
2020-05-19 23:49:07 +0200

    chore(nextbike/io/output): change name + add param of method + prints

--- nextbike/io/output.py
+++ nextbike/io/output.py
@@ -3,14 +3,17 @@ import os
 import pickle
 
 
-def save_model(model):
-    pickle.dump(model, open(os.path.join(get_data_path(), "output/model.pkl"), 'wb'))
+def save_object(pObject, pFilename):
+    pickle.dump(pObject, open(os.path.join(get_data_path(), "output", "models", pFilename), 'wb'))
+    print("Saved", pFilename)
 
 
+# TODO: No need for save_fig and saveTrip => Create just one method
 def saveTrip(df):
-    # Save final df
     df.to_csv(os.path.join(get_data_path(), "output/Trips.csv"))
+    print("Saved Trips.csv")
 
 
 def save_fig(fig, file_name):
     fig.savefig(os.path.join(get_data_path(), "output/", file_name))
+    print("Saved", file_name)
