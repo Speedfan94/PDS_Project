2959563200c827654e40659d25946943390a6ec5
Kevin W <kevin.blau2@gmail.com>
2020-05-19 23:54:20 +0200

    feat(nextbike/prediction/training): add train regression + placeholders

--- nextbike/prediction/training.py
+++ nextbike/prediction/training.py
@@ -1,3 +1,18 @@
+from sklearn.linear_model import LinearRegression
+from .. import io
 
-def train(X_train, y_train):
+
+def train_linear_regression(pX_train_scaled, py_train):
+    lin = LinearRegression()
+    lin.fit(pX_train_scaled, py_train)
+    io.save_object(lin, "Linear_Regression_Model.pkl")
+    # Uncommend for showing
+    # print("Intercept=", lin.intercept_, "Coefficients=", lin.coef_)
+
+
+def train_neural_network(X_train, y_train):
+    print()
+
+
+def train_svm(X_train, y_train):
     print()
