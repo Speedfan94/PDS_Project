fab21f4d64c544f03166774e82c24d86c83fce25
weisske <63042868+weisske@users.noreply.github.com>
2020-05-21 15:09:11 +0200

    Update utils.py
    Added comment to remember on Docstring

--- nextbike/utils.py
+++ nextbike/utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-
+# TODO: add docstring
 def cast_datetime(pDf, datetime_columns, as_numeric_value=False):
     for column in datetime_columns:
         pDf[column] = pd.to_datetime(pDf[column], format="%Y-%m-%d %H:%M:%S")
