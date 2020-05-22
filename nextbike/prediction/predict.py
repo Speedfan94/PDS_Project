631a692d489561ab769ebf3c97da5c3f7e426ee2
Kevin W <kevin.blau2@gmail.com>
2020-05-23 01:29:19 +0200

    feat(nextbike/prediction): add neural network train and predict + add docstrings

--- nextbike/prediction/predict.py
+++ nextbike/prediction/predict.py
@@ -1,28 +1,38 @@
 from .. import io
 from sklearn import metrics
+from tensorflow.keras.models import load_model
 import numpy as np
 
 
 def predict_by_regression(pX_test, py_test):
     scaler = io.read_object("Standard_Scaler.pkl")
     model = io.read_object("Linear_Regression_Model.pkl")
-    pca = io.read_object("PCA.pkl")
+    #pca = io.read_object("PCA.pkl")
     X_test_scaled = scaler.transform(pX_test)
-    X_test_transformed = pca.transform(X_test_scaled)
-    y_predictions = model.predict(X_test_transformed)
+    #X_test_transformed = pca.transform(X_test_scaled)
+    y_predictions = model.predict(X_test_scaled)
     # TODO: Is it fine to call this method from here or should I return to cli.py and then evaluate metrics?
     show_errors_metrics(py_test, y_predictions, "Linear_Regression_Prediction.png")
 
 
-def predict_by_svm(pX_test, py_test):
+def predict_by_nn(pX_test, py_test):
     scaler = io.read_object("Standard_Scaler.pkl")
+    model = load_model(io.get_path("Neural_Network_Model", "output", "models"))
     pca = io.read_object("PCA.pkl")
     X_test_scaled = scaler.transform(pX_test)
-    X_test_transformed = pca.transform(X_test_scaled)
-    for i in range(4):
-        model = io.read_object("SVM_Regression_Model_"+str(i)+".pkl")
-        y_predictions = model.predict(X_test_transformed)
-        show_errors_metrics(py_test, y_predictions, "SVM_Regression_Prediction_"+str(i))
+    #X_test_transformed = pca.transform(X_test_scaled)
+    y_predictions = model.predict(X_test_scaled)
+    show_errors_metrics(py_test, y_predictions, "NN_Regression_Model")
+
+
+def predict_by_svm(pX_test, py_test):
+    scaler = io.read_object("Standard_Scaler.pkl")
+    #pca = io.read_object("PCA.pkl")
+    X_test_scaled = scaler.transform(pX_test)
+    #X_test_transformed = pca.transform(X_test_scaled)
+    model = io.read_object("SVM_Regression_Model_"+str(3)+".pkl")
+    y_predictions = model.predict(X_test_scaled)
+    show_errors_metrics(py_test, y_predictions, "SVM_Regression_Prediction_"+str(3))
 
 
 def show_errors_metrics(py_true, py_predictions, pFilename):
--- nextbike/prediction/training.py
+++ nextbike/prediction/training.py
@@ -1,7 +1,12 @@
-from sklearn.linear_model import LinearRegression
-from sklearn import svm
+import pandas as pd
 from .. import io
 import warnings
+
+from sklearn.linear_model import LinearRegression
+from sklearn import svm
+from tensorflow import keras
+from tensorflow.keras import layers
+
 
 def train_linear_regression(pX_train_scaled, py_train):
     lin = LinearRegression()
@@ -9,20 +14,45 @@ def train_linear_regression(pX_train_scaled, py_train):
     io.save_object(lin, "Linear_Regression_Model.pkl")
 
 
-def train_neural_network(X_train, y_train):
-    print()
+def train_neural_network(pX_train_scaled, py_train):
+    """train neural network
+
+    Args:
+        pX_train_scaled (DataFrame): Scaled x input of train set
+        py_train (Series): y output to train on
+    Returns:
+        no return
+    """
+    neural_network = keras.Sequential(
+        [layers.Dense(36, activation="relu", input_shape=[pX_train_scaled.shape[1]]),
+         # layers.Dropout(0.2),
+         layers.Dense(36, activation="relu"),
+         # layers.Dropout(0.2),
+         layers.Dense(1)])
+    optimizer = keras.optimizers.RMSprop(0.001)
+    neural_network.compile(loss="mse",
+                           optimizer=optimizer,
+                           metrics=["mae", "mse"])
+    epochs = 10
+    batch_size = 200  # right now not used but should be tried
+    neural_network.fit(pX_train_scaled, py_train.values, epochs=epochs, validation_split=0.2)
+    neural_network.save(io.get_path("Neural_Network_Model", "output", "models"))
 
 
 def train_svm(pX_train_scaled, py_train):
+    """Trains a svm
+
+    Args:
+        pX_train_scaled (DataFrame): Scaled x input of train set
+        py_train (Series): y output to train on
+    Returns:
+        no return
+    """
     warnings.filterwarnings('ignore', 'Solver terminated early.*')
     # specify kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}, default=’rbf’
     # degreeint, default=3
     # max_iterint, default=-1 => no limit
     # verbose=1
-    for i in range(4):
-        print("Train SVM of degree "+str(i))
-        regr = svm.SVR(max_iter=1000, cache_size=2000, degree=i)
-        regr.fit(pX_train_scaled, py_train)
-        io.save_object(regr, "SVM_Regression_Model_"+str(i)+".pkl")
-
-
+    regr = svm.SVR(max_iter=1000, cache_size=2000, degree=3)
+    regr.fit(pX_train_scaled, py_train)
+    io.save_object(regr, "SVM_Regression_Model_" + str(3) + ".pkl")
