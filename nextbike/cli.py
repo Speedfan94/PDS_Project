96f34532659a1c3cb9609145cff22422f4954285
Kevin W <kevin.blau2@gmail.com>
2020-05-26 16:29:54 +0200

    fix(nextbike.cli.py): small bug fixed

--- nextbike/cli.py
+++ nextbike/cli.py
@@ -89,7 +89,6 @@ def features():
     print("Visualize correlations...")
     df_features_2 = prediction.math_prepare_feature.drop_features(df_features_2)
     visualization.math.corr_analysis(df_features_2)
-    df_features_2 = prediction.math_prepare_feature.drop_features(df_features_2)
     io.output.save_csv(df_features_2, "Features.csv")
     # visualization.math.plot_features_influence(df_features_2)
 
