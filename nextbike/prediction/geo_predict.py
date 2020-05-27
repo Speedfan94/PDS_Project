f40b54da75e752d814ae65594e04a717b11b2b1a
Kevin W <kevin.blau2@gmail.com>
2020-05-27 15:51:08 +0200

    feat(nextbike/prediction/geo_predict): add feature + printing + test_size

--- nextbike/prediction/geo_predict.py
+++ nextbike/prediction/geo_predict.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -6,10 +7,10 @@ from sklearn.neighbors import KNeighborsClassifier
 # TODO: very slow and only a first prototype
 # TODO: Add docstring
 def train_pred(p_df):
-    X = p_df[['Duration', 'Dist_start']]
-    y = p_df['Direction']
+    X = p_df[["Duration", "Dist_start", "Bike Number"]]
+    y = p_df["Direction"]
     X = StandardScaler().fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     clf = KNeighborsClassifier()
     clf.fit(X_train, y_train)
-    print('Test score (mean accuracy)', clf.score(X_test, y_test))
+    print("Test score (mean accuracy)", np.round(clf.score(X_test, y_test)*100, 2), "%")
