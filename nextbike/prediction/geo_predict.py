3d6dc3f9832f5c72fe40b0bfffad1cba7cfd9b5b
Kevin W <kevin.blau2@gmail.com>
2020-06-02 15:07:03 +0200

    chore(nextbike/prediction): insert docstrings + small style changes
     change ordner of  read pickle files in math_predict
    
    change name of SVM models in math_train

--- nextbike/prediction/geo_predict.py
+++ nextbike/prediction/geo_predict.py
@@ -4,9 +4,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 
-# TODO: very slow and only a first prototype
-# TODO: Add docstring
 def train_pred(p_df):
+    """Train and predict KNN Classifier for trip direction.
+
+    Train and predict if a trips end is nearer the university than the start.
+    Print the accuracy of the prdiction.
+    Args:
+        p_df (DataFrame): Dataframe of trip data
+    Returns:
+        no Return
+    """
     X = p_df[["Duration", "Dist_start", "Bike Number"]]
     y = p_df["Direction"]
     X = StandardScaler().fit_transform(X)
@@ -14,3 +21,6 @@ def train_pred(p_df):
     clf = KNeighborsClassifier()
     clf.fit(X_train, y_train)
     print("Test score (mean accuracy)", np.round(clf.score(X_test, y_test)*100, 2), "%")
+
+
+# TODO: add additional methods for prediction
--- nextbike/prediction/math_predict.py
+++ nextbike/prediction/math_predict.py
@@ -5,42 +5,89 @@ from tensorflow.keras.models import load_model
 import numpy as np
 
 
-# TODO: Add docstring
 def predict_by_regression(p_X_test, p_y_test):
+    """Predicts the duration of a trip by trained Linear Regression model.
+
+    This method uses the trained scaler, pca and linear regression model to predict the given test set duration.
+    Then a method to evaluate the performance is called.
+    Args:
+        p_X_test (DataFrame): Dataframe of input features for prediction (matrix)
+        p_y_test (Series): Series of true output durations (vector)
+    Returns:
+        no Return
+    """
+    # Read pickle objects
     scaler = io.read_object("Standard_Scaler.pkl")
-    model = io.read_object("Linear_Regression_Model.pkl")
     pca = io.read_object("PCA.pkl")
+    model = io.read_object("Linear_Regression_Model.pkl")
+    # Use trained pickle objects
     X_test_scaled = scaler.transform(p_X_test)
     X_test_transformed = pca.transform(X_test_scaled)
     y_predictions = model.predict(X_test_transformed)
-    # TODO: Is it fine to call this method from here or should I return to cli.py and then evaluate metrics?
+    # Evaluate prediction
     show_errors_metrics(p_y_test, y_predictions, "01_Linear_Regression_Prediction")
 
 
-# TODO: Add docstring
 def predict_by_nn(p_X_test, p_y_test):
+    """Predicts the duration of a trip by trained neural network model.
+
+    This method uses the trained scaler, pca and Neural Network model to predict the given test set duration.
+    Then a method to evaluate the performance is called.
+    Args:
+        p_X_test (DataFrame): Dataframe of input features for prediction (matrix)
+        p_y_test (Series): Series of true output durations (vector)
+    Returns:
+        no Return
+    """
+    # Read pickle objects
     scaler = io.read_object("Standard_Scaler.pkl")
-    model = load_model(io.get_path("Neural_Network_Model", "output", "models"))
     pca = io.read_object("PCA.pkl")
+    model = load_model(io.get_path("Neural_Network_Model", "output", "models"))
+    # Use trained pickle objects
     X_test_scaled = scaler.transform(p_X_test)
     X_test_transformed = pca.transform(X_test_scaled)
     y_predictions = model.predict(X_test_transformed)
+    # Evaluate prediction
     show_errors_metrics(p_y_test, y_predictions, "03_NN_Regression_Model")
 
 
-# TODO: Add docstring
 def predict_by_svm(p_X_test, p_y_test):
+    """Predicts the duration of a trip by trained Support Vector Machine model.
+
+    This method uses the trained scaler, pca and Support Vector Machine model to predict the given test set duration.
+    Then a method to evaluate the performance is called.
+    Args:
+        p_X_test (DataFrame): Dataframe of input features for prediction (matrix)
+        p_y_test (Series): Series of true output durations (vector)
+    Returns:
+        no Return
+    """
+    # Read pickle objects
     scaler = io.read_object("Standard_Scaler.pkl")
     pca = io.read_object("PCA.pkl")
+    model = io.read_object("SVM_Regression_Model_3.pkl")
+    # Use trained pickle objects
     X_test_scaled = scaler.transform(p_X_test)
     X_test_transformed = pca.transform(X_test_scaled)
-    model = io.read_object("SVM_Regression_Model_"+str(3)+".pkl")
     y_predictions = model.predict(X_test_transformed)
-    show_errors_metrics(p_y_test, y_predictions, "02_SVM_Regression_Prediction_" + str(3))
+    # Evaluate prediction
+    show_errors_metrics(p_y_test, y_predictions, "02_SVM_Regression_Prediction_3")
 
 
-# TODO: Add docstring
 def show_errors_metrics(p_y_true, p_y_predictions, p_filename):
+    """Evaluate the error metrics for a prediction by a model.
+
+    This method is called after the prediction of a given model. It evaluates the performance of the prediction by
+        - Root Mean Squared Error
+        - Mean Absolute Error
+    Then a visualization method is called to visualize the performance of the prediction of the given model.
+    Args:
+        p_y_true (Series): Series of true output durations (vector)
+        p_y_predictions (Series): Series of predicted output durations (vector)
+        p_filename (str): String with the model name
+    Returns:
+        no Return
+    """
     print(p_filename, "Test loss - Error Metrics:")
     print("RMSE:", np.sqrt(metrics.mean_squared_error(p_y_true, p_y_predictions)), end=" ")
     print("MAE", metrics.mean_absolute_error(p_y_true, p_y_predictions))
