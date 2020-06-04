c9b1a3176088907376ce6eac48790f1698c668bd
Kevin W <kevin.blau2@gmail.com>
2020-06-05 01:03:55 +0200

    fix(nextbike/prediction/geo_predict): solve logical failures

--- nextbike/prediction/geo_predict.py
+++ nextbike/prediction/geo_predict.py
@@ -20,33 +20,42 @@ def train_pred(p_df):
         no Return
     """
     # data
-    X = p_df[["Duration", "Dist_start"]]
+    X = p_df.drop("Direction", axis=1)
     y = p_df["Direction"]
-    X_scaled = StandardScaler().fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
     # train
     # KNN
+    print("KNN")
     clf = KNeighborsClassifier(n_neighbors=20, weights="distance")
-    clf.fit(X_train, y_train)
+    clf.fit(X_train_scaled, y_train)
     # Decision Tree
+    print("Decision Tree")
     dt = DecisionTreeClassifier(max_depth=5)
-    dt.fit(X_train, y_train)
+    dt.fit(X_train_scaled, y_train)
     # RF
+    print("Random Forest")
     rf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)
-    rf.fit(X_train, y_train)
+    rf.fit(X_train_scaled, y_train)
     # NN
+    print("Neural Network")
     nn = MLPClassifier(alpha=1, max_iter=1000)
-    nn.fit(X_train, y_train)
+    nn.fit(X_train_scaled, y_train)
     # GNB
-    gnb = GaussianNB()
-    gnb.fit(X_train, y_train)
+    #print("Gaussian Naive Bayes")
+    #gnb = GaussianNB()
+    #gnb.fit(X_train_scaled, y_train)
     # predict
-    print("KNN: Test score (mean accuracy)", np.round(clf.score(X_test, y_test) * 100, 2), "%")
-    print("DT: Test score (mean accuracy)", np.round(dt.score(X_test, y_test) * 100, 2), "%")
-    print("RF: Test score (mean accuracy)", np.round(rf.score(X_test, y_test) * 100, 2), "%")
-    print("NN: Test score (mean accuracy)", np.round(nn.score(X_test, y_test) * 100, 2), "%")
-    print("GNB: Test score (mean accuracy)", np.round(gnb.score(X_test, y_test) * 100, 2), "%")
-    visualization.math_predictive.plot_direction_classification(X, y)
+    X_test_scaled = scaler.transform(X_test)
+    print("KNN: Test score (mean accuracy)", np.round(clf.score(X_test_scaled, y_test) * 100, 2), "%")
+    print("DT: Test score (mean accuracy)", np.round(dt.score(X_test_scaled, y_test) * 100, 2), "%")
+    print("RF: Test score (mean accuracy)", np.round(rf.score(X_test_scaled, y_test) * 100, 2), "%")
+    print("NN: Test score (mean accuracy)", np.round(nn.score(X_test_scaled, y_test) * 100, 2), "%")
+    #print("GNB: Test score (mean accuracy)", np.round(gnb.score(X_test_scaled, y_test) * 100, 2), "%")
+    # TODO: Visualization of 1-dimension?
+    #visualization.math_predictive.plot_direction_classification(X, y)
 
 
 # TODO: add additional methods for prediction
