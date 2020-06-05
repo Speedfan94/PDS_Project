f8600b6d59ac3f98d9ed14de38e2b0083ec65fed
Max Strohbuecker <max@strohbuecker.koeln>
2020-06-05 16:25:34 +0200

    fix(nextbike/io): set index manually as the index_col option of read_csv produces numpy warnings

--- nextbike/io/input.py
+++ nextbike/io/input.py
@@ -18,7 +18,10 @@ def read_csv(p_filename, p_io_folder, p_sub_folder=""):
     """
     path = os.path.join(get_data_path(), p_io_folder, p_sub_folder, p_filename)
     try:
-        df = pd.read_csv(path, index_col=0)
+        # index_col=0 throws numpy warning,
+        # so we set it manually after reading in the file
+        df = pd.read_csv(path)
+        df.set_index(df.columns[0], inplace=True)
         print("Read:", path)
         return df
     except FileNotFoundError:
