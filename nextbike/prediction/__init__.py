353599b2b7942cb7a74165db06c43b3c18a107ab
Kevin W <kevin.blau2@gmail.com>
2020-06-07 00:59:56 +0200

    feat(nextbike/prediction/__init__): add new files to init

--- nextbike/prediction/__init__.py
+++ nextbike/prediction/__init__.py
@@ -1,6 +1,11 @@
-from .math_train import *
-from .math_split import *
-from .math_predict import *
-from .math_prepare_feature import *
-from .geo_predict import *
+from .prepare_feature import *
 from .add_weather_data import *
+from .split import *
+
+from .math_train import *
+from .math_predict import *
+
+from .geo_train import *
+from .geo_predict import *
+
+from .evaluate import *
