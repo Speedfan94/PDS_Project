8a7d64f53f4f34e7208fb1ddf08aae3e00da7894
Max Strohbuecker <max@strohbuecker.koeln>
2020-06-06 16:33:04 +0200

    feat(nextbike/datapreparation): tried out distance calculation with geopandas and manually calculation method, but both calculated different distances, so we stick to the old geopy solution

--- nextbike/datapreparation/feature_add.py
+++ nextbike/datapreparation/feature_add.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import geopy.distance as geodis
+import numpy as np
+
+# imports needed for alternative distance methods
+# import geopandas as gpd
+# from shapely.geometry import Point
 
 
 def additional_feature_creation(p_df_trips):
@@ -57,10 +62,85 @@ def quick_create_dist(p_df):
     Returns:
         p_df (DataFrame): DataFrame with added columns [Dist_start, Dist_end, Direction]
     """
-    uni = (49.452210, 11.079575)
-    p_df['Dist_start'] = p_df.apply(lambda row: geodis.distance((row['Latitude_start'],
-                                                                 row['Longitude_start']), uni).km, axis=1)
-    p_df['Dist_end'] = p_df.apply(lambda row: geodis.distance((row['Latitude_end'],
-                                                               row['Longitude_end']), uni).km, axis=1)
+
+    """
+    # ===== METHOD 1: Geopandas =====
+    
+    uni = Point(11.079575, 49.452210)
+    # uni_geometry = [Point(11.079575, 49.452210)]
+    # gdf_uni = gpd.GeoDataFrame(geometry=uni, crs={'init':'epsg:4326', 'unit':'m'})
+    # gdf_uni = gdf_uni.to_crs(epsg=3395)
+    # gdf_uni = gdf_uni.to_crs(epsg=32633)
+
+    start_pos = gpd.points_from_xy(p_df["Longitude_start"], p_df["Latitude_start"])
+    end_pos = gpd.points_from_xy(p_df["Longitude_end"], p_df["Latitude_end"])
+    gdf_start_pos = gpd.GeoDataFrame(p_df, geometry=start_pos, crs={'init':'epsg:4326', 'units':'m'})
+    # gdf_start_pos = gdf_start_pos.to_crs(epsg=25832)
+    # gdf_start_pos = gdf_start_pos.to_crs(epsg=3310)
+    # gdf_start_pos = gdf_start_pos.to_crs(epsg=3395)
+    # gdf_start_pos = gdf_start_pos.to_crs(epsg=32633)
+    print(gdf_start_pos)
+    gdf_end_pos = gpd.GeoDataFrame(p_df, geometry=end_pos, crs={'init':'epsg:4326', 'units':'m'})
+    # gdf_end_pos = gdf_end_pos.to_crs(epsg=25832)
+    # gdf_end_pos = gdf_end_pos.to_crs(epsg=3310)
+    # gdf_end_pos = gdf_end_pos.to_crs(epsg=3395)
+    # gdf_end_pos = gdf_end_pos.to_crs(epsg=32633)
+    print(gdf_end_pos)
+    p_df["Dist_start"] = gdf_start_pos.distance(uni)*6373
+    p_df["Dist_end"] = gdf_end_pos.distance(uni)*6373
+    p_df["Direction"] = p_df["Dist_start"] > p_df["Dist_end"]
+    print(p_df[["Dist_start", "Dist_end", "Direction"]])
+    
+    # ===== Troubles with converting coordinates to km, even with different epsg codes =====
+    # ===== So direction is correct, but distances are crappy values =====
+    """
+
+    """
+    # ===== METHOD 2: Manually calculate distances =====
+
+    uni_lng = 11.079575
+    uni_lat = 49.452210
+    p_df["Dist_start"] = haversine_vectorize(p_df["Longitude_start"], p_df["Latitude_start"], uni_lng, uni_lat)
+    p_df["Dist_end"] = haversine_vectorize(p_df["Longitude_end"], p_df["Latitude_end"], uni_lng, uni_lat)
+    p_df["Direction"] = p_df["Dist_start"] > p_df["Dist_end"]
+    print(p_df[["Dist_start", "Dist_end", "Direction"]])
+
+    # ===== Much more performant, but slightly different distances, seems to calculate wrong values =====
+    """
+
+    # ===== METHOD 3: Geopy =====
+
+    uni = (11.079575, 49.452210)
+    p_df['Dist_start'] = p_df.apply(lambda row: geodis.distance((row['Longitude_start'],
+                                                                 row['Latitude_start']), uni).km, axis=1)
+    p_df['Dist_end'] = p_df.apply(lambda row: geodis.distance((row['Longitude_end'],
+                                                               row['Latitude_end']), uni).km, axis=1)
     p_df['Direction'] = p_df['Dist_start'] > p_df['Dist_end']  # to uni: True, away: False
+    print(p_df[["Dist_start", "Dist_end", "Direction"]])
+
+    # ===== Most inperformant, but most precise and simple solution =====
+
     return p_df
+
+
+def haversine_vectorize(lon1, lat1, lon2, lat2):
+    """ Calculates manually distances between two points by longitude and latitude, returns distance in km
+
+    Args:
+        lon1: longitude of first point
+        lat1: latitude of first point
+        lon2: longitude of second point
+        lat2: latitude of second point
+    Returns:
+        distance between points in km
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    newlon = lon2 - lon1
+    newlat = lat2 - lat1
+
+    haver_formula = np.sin(newlat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(newlon / 2.0) ** 2
+
+    dist = 2 * np.arcsin(np.sqrt(haver_formula))
+    km = 6367 * dist  # 6367 for distance in KM for miles use 3958
+    return km
