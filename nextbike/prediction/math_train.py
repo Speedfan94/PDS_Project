71ec21d91fb50389a0f820500789e7118c0b7555
Kevin W <kevin.blau2@gmail.com>
2020-06-07 13:09:57 +0200

    feat(nextbike/prediction/math_train): add validation feature

--- nextbike/prediction/math_train.py
+++ nextbike/prediction/math_train.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn import svm
 from tensorflow import keras
 from tensorflow.keras import layers
-from sklearn import metrics
+from sklearn.model_selection import train_test_split
 
 
 def train_linear_regression(p_X_train_scaled, p_y_train):
@@ -20,12 +20,15 @@ def train_linear_regression(p_X_train_scaled, p_y_train):
     Returns:
         no return
     """
+    # create a validation set which is 20% of the whole dataset. Therefore use formula to receive ca. 0.2857.
+    X_train, X_val, y_train, y_val = train_test_split(p_X_train_scaled, p_y_train, random_state=42, test_size=0.2/0.7)
     lin = LinearRegression()
-    lin.fit(p_X_train_scaled, p_y_train)
+    lin.fit(X_train, y_train)
     # print("Coefficients: ", lin.coef_)
     io.save_object(lin, "Linear_Regression_Model.pkl")
-    y_prediction = lin.predict(p_X_train_scaled)
-    return y_prediction
+    y_prediction_train = lin.predict(X_train)
+    y_prediction_val = lin.predict(X_val)
+    return [y_train, y_val, y_prediction_train, y_prediction_val]
 
 
 def train_neural_network(p_X_train_scaled, p_y_train, p_testing=False):
@@ -47,6 +50,8 @@ def train_neural_network(p_X_train_scaled, p_y_train, p_testing=False):
     Returns:
         no return
     """
+    # create a validation set which is 20% of the whole dataset. Therefore use formula to receive ca. 0.2857.
+    X_train, X_val, y_train, y_val = train_test_split(p_X_train_scaled, p_y_train, random_state=42, test_size=0.2 / 0.7)
     neural_network = keras.Sequential(
         [layers.Dense(36, activation="relu", input_shape=[p_X_train_scaled.shape[1]]),
          # layers.Dropout(0.2),
@@ -64,15 +69,17 @@ def train_neural_network(p_X_train_scaled, p_y_train, p_testing=False):
                            optimizer=optimizer,
                            metrics=["mae", "mse"])
     epochs = 20
-    # batch_size = 200  # right now not used but should be tried
-    history = neural_network.fit(p_X_train_scaled, p_y_train.values, epochs=epochs, validation_split=0.2)
+    # create a validation set which is 20% of the whole dataset. Therefore use formula to receive ca. 0.2857.
+    history = neural_network.fit(X_train, y_train, epochs=epochs, validation_data=(X_val, y_val))
     if p_testing:
         neural_network.save(io.get_path("TEST_Neural_Network_Regression_Model", "output", "models"))
     else:
         neural_network.save(io.get_path("Neural_Network_Regression_Model", "output", "models"))
-    y_prediction = neural_network.predict(p_X_train_scaled)
+    y_prediction_train = neural_network.predict(X_train)
+    y_prediction_val = neural_network.predict(X_val)
     visualization.math_predictive.plot_train_loss(history)
-    return y_prediction
+    nn_regression_sets = [y_train, y_val, y_prediction_train, y_prediction_val]
+    return nn_regression_sets
 
 
 def train_svm(p_X_train_scaled, p_y_train):
@@ -90,15 +97,19 @@ def train_svm(p_X_train_scaled, p_y_train):
         p_X_train_scaled (DataFrame): Scaled X input of train set (matrix)
         p_y_train (Series): y output to train on (vector)
     Returns:
-        no return
+        svm_regression_sets (List): list of y_train, y_val, y_prediction_train, y_prediction_val
     """
+    # create a validation set which is 20% of the whole dataset. Therefore use formula to receive ca. 0.2857.
+    X_train, X_val, y_train, y_val = train_test_split(p_X_train_scaled, p_y_train, random_state=42, test_size=0.2 / 0.7)
     warnings.filterwarnings('ignore', 'Solver terminated early.*')
     # specify kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}, default=’rbf’
     # degreeint, default=3
     # max_iterint, default=-1 => no limit
     # verbose=1
-    regr = svm.SVR(kernel="linear", max_iter=1000, cache_size=2000, degree=3, gamma="auto")  # max_iter=5000 lot better
-    regr.fit(p_X_train_scaled, p_y_train)
-    io.save_object(regr, "SVM_Regression_Model_3.pkl")
-    y_prediction = regr.predict(p_X_train_scaled)
-    return y_prediction
+    svm_regr = svm.SVR(kernel="poly", max_iter=1000, cache_size=2000, degree=3, gamma="auto")
+    svm_regr.fit(X_train, y_train)
+    io.save_object(svm_regr, "SVM_Regression_Model_3.pkl")
+    y_prediction_train = svm_regr.predict(X_train)
+    y_prediction_val = svm_regr.predict(X_val)
+    svm_regression_sets = [y_train, y_val, y_prediction_train, y_prediction_val]
+    return svm_regression_sets
