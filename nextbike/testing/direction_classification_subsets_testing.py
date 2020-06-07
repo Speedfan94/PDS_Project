0a5d8a87a65f1899bda5fe4d2aba2e99ff6d2eb0
Kevin W <kevin.blau2@gmail.com>
2020-06-07 16:00:43 +0200

    fix(nextbike\testing\direction_classification_subsets_testing): fix evaluation on different subsets
    before also trained on these subsets.

--- nextbike/testing/direction_classification_subsets_testing.py
+++ nextbike/testing/direction_classification_subsets_testing.py
@@ -1,8 +1,7 @@
 from .. import prediction
+from .. import io
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.dummy import DummyClassifier
 
 
 def filter_subsets(p_df):
@@ -20,36 +19,23 @@ def filter_subsets(p_df):
 
 
 def test_subset_classification(p_df_subset):
-
-    print("Split Data...")
-    X_train, X_test, y_train, y_test = prediction.split.simple_split_direction(p_df_subset)
+    X = p_df_subset.drop("Direction", axis=1)
+    y = p_df_subset["Direction"]
     print("Scale Data...")
     scaler = StandardScaler()
-    X_scaled_train = scaler.fit_transform(X_train)
+    X_scaled_train = scaler.fit_transform(X)
     print("Do PCA...")
     pca = PCA(n_components=15)
     pca.fit(X_scaled_train)
     X_train_transformed = pca.transform(X_scaled_train)
-    # Train
-    print("Train Dummy Classifier...")
-    dummy = DummyClassifier(strategy="most_frequent")
-    dummy.fit(X_train_transformed, y_train)
-    dummy_y_prediction = dummy.predict(X_train_transformed)
-    print("Train KNeighbors Classifier...")
-    clf = KNeighborsClassifier(n_neighbors=20, weights="distance")
-    clf.fit(X_train_transformed, y_train)
-    kn_y_prediction = clf.predict(X_train_transformed)
-    # Evaluate Training
-    # prediction.evaluate.direction_error_metrics(y_train, dummy_y_prediction, "Dummy_Classifier")
-    # prediction.evaluate.direction_error_metrics(y_train, kn_y_prediction, "KNeighbors_Classifier")
-    # Prepare
-    X_test_scaled = scaler.transform(X_test)
-    X_test_transformed = pca.transform(X_test_scaled)
+    # Read Models
+    dummy = io.read_object("Dummy_classifier_model.pkl")
+    clf = io.read_object("KNearestNeighbours_classifier_model.pkl")
     # Predict
-    print("Predict")
-    dummy_y_prediction = dummy.predict(X_test_transformed)
-    kn_y_prediction = clf.predict(X_test_transformed)
+    print("Predict...")
+    dummy_y_prediction = dummy.predict(X_train_transformed)
+    kn_y_prediction = clf.predict(X_train_transformed)
     # Evaluate Predict
-    prediction.evaluate.direction_error_metrics(y_test, dummy_y_prediction, "Dummy_Classifier", "Testing")
-    prediction.evaluate.direction_error_metrics(y_test, kn_y_prediction, "KNeighbors_Classifier", "Testing")
+    prediction.evaluate.direction_error_metrics(y, dummy_y_prediction, "Dummy_Classifier", "Testing")
+    prediction.evaluate.direction_error_metrics(y, kn_y_prediction, "KNeighbors_Classifier", "Testing")
 
